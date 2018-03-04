5d7fccbcf38d3e2c7fbbfd1fdb81c833369f073e
kunal <kunalgk1999@gmail.com>
2018-03-04 14:21:32 +0530

    added working user-authentication login and logout

--- student-dashboard/urls.py
+++ student-dashboard/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-
+    url(r'^accounts/', include('accounts.url'), name="accounts"),
     url(r'^', include('subject.urls')),
 ]
